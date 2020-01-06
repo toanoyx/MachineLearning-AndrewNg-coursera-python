9aabb8bc2652e19c773058a38f883a7c26f52527
toanoyx <38813080+toanoyx@users.noreply.github.com>
2020-01-06 16:19:40 +0800

    Create gradient_descent_multi.py

---
+++ ex1 Linear Regression/ex1_multi/gradient_descent_multi.py
@@ -0,0 +1,14 @@
+import numpy as np
+from compute_cost import *
+
+
+def gradient_descent_multi(X, y, theta, alpha, num_iters):
+    m = y.size  # 样本数
+    J_history = np.zeros(num_iters)  # 每一次迭代都有一个代价函数值
+    y = y.reshape(-1, 1)  # 用2维数组表示列向量 (m,1)
+
+    for i in range(0, num_iters):  # num_iters次迭代优化
+        theta = theta - (alpha / m) * X.T.dot(h(X, theta) - y)
+        J_history[i] = compute_cost(X, y, theta)  # 用每一次迭代产生的参数 来计算代价函数值
+
+    return theta, J_history
