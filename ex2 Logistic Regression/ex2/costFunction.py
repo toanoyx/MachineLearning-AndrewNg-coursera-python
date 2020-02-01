443270c61e1363bbcecbaf165116a5650bd14c8b
toanoyx <38813080+toanoyx@users.noreply.github.com>
2020-02-01 13:14:41 +0800

    Create costFunction.py

---
+++ ex2 Logistic Regression/ex2/costFunction.py
@@ -0,0 +1,10 @@
+from sigmoid import *
+
+
+def costFunction(theta, X, y):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+    first = np.multiply(-y, np.log(sigmoid(X * theta.T)))
+    second = np.multiply((1 - y), np.log(1 - sigmoid(X * theta.T)))
+    return np.sum(first - second) / (len(X))
