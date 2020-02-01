88882bba8a3265b49a78ff5085dcf4bbab144995
toanoyx <38813080+toanoyx@users.noreply.github.com>
2020-02-01 13:15:41 +0800

    Create gradientDescent.py

---
+++ ex2 Logistic Regression/ex2/gradientDescent.py
@@ -0,0 +1,19 @@
+import numpy as np
+from sigmoid import *
+
+
+def gradientDescent(theta, X, y):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+
+    parameters = int(theta.ravel().shape[1])
+    grad = np.zeros(parameters)
+
+    error = sigmoid(X * theta.T) - y
+
+    for i in range(parameters):
+        term = np.multiply(error, X[:, i])
+        grad[i] = np.sum(term) / len(X)
+
+    return grad
