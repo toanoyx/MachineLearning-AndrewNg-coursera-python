0d81c824680c433342e1e09a2bd8214ca263eb62
toanoyx <38813080+toanoyx@users.noreply.github.com>
2020-02-05 20:24:19 +0800

    Create gradientReg.py

---
+++ ex2 Logistic Regression/ex2_reg/gradientReg.py
@@ -0,0 +1,18 @@
+import numpy as np
+from sigmoid import *
+
+
+def gradientReg(theta, X, y, learningRate):
+    theta = np.matrix(theta)
+    X = np.matrix(X)
+    y = np.matrix(y)
+    parameters = int(theta.ravel().shape[1])
+    grad = np.zeros(parameters)
+    error = sigmoid(X * theta.T) - y
+    for i in range(parameters):
+        term = np.multiply(error, X[:, i])
+        if i == 0:
+            grad[i] = np.sum(term) / len(X)
+        else:
+            grad[i] = (np.sum(term) / len(X)) + ((learningRate / len(X)) * theta[:, i])
+    return grad
